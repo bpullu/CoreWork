19f47ac7bfb50067a6e175e9a94439ec75c94e1e
Buğra Pullu <bugrapullu@gmail.com>
2017-07-31 15:01:24 +0300

    Revert "xzczxc"
    This reverts commit 11deb98befc3046d454c98cff94141b36fa6064e.

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,8 +1,12 @@
-﻿namespace ClassLibrary1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
 {
     public class Class1
     {
-
-
     }
 }
