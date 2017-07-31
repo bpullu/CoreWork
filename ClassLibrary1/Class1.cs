11deb98befc3046d454c98cff94141b36fa6064e
Buğra Pullu <bugrapullu@gmail.com>
2017-07-31 14:55:55 +0300

    xzczxc

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassLibrary1
+﻿namespace ClassLibrary1
 {
     public class Class1
     {
+
+
     }
 }
